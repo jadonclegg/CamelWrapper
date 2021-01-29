59bcb2367d1c53560c8c9a85c04fe3eb47c91e47
jkcoxson <jkcoxson@gmail.com>
2021-01-28 22:31:07 -0700

    This should work idk

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 ////////////////////////////////
 /// CamelWrapper by jkcoxson ///
+///  Pls don't steal license ///
 ////////////////////////////////
 
 // Javascript > Java
@@ -11,11 +12,12 @@ const { spawn } = require("child_process");
 const config = require("./config.json");
 const Net = require('net');
 
-const serverConnection = new Net.Socket();
+var serverConnection = new Net.Socket();
 
 var connected = false
 
 function connectpls(){
+    //serverConnection = new Net.Socket();
     serverConnection.connect({port: config.mother_port,host: config.IP}, () =>{
         //Nothing goes in here, so that's cool
     });
@@ -30,61 +32,101 @@ function sendMessage(toSend){
     }
 }
 
-function packetInterpreter(raw){
+
+
+
+async function packetInterpreter(raw){
     var packet = null
     try {
         // If packet parses as JSON, continue. Otherwise exit
         packet = JSON.parse(raw)
+        // Test if it has a packet type, or discard
+        var temp = packet.packet
     } catch{
+        console.log('Recieved bad packet, dropped')
         return
     }
     if (packet.type=="playerList"){
-        // Run the command in the java server and send it over
+        var toSend = {
+            "packet":"playerlist",
+            "list":await(getPlayersOnline()).split('\n')
+        }
+        if (connected){
+            serverConnection.write(JSON.stringify(toSend));
+        }
+
     }
-    if (packet.type=="coordsOf"){
-        // Run java command to get packet.player then sendMessage(x,y,z)
+    if (packet.type=="reqCoords"){
+        var toSend = {
+            "packet":"reqCoords",
+            "list":[]
+        }
+        var idkWhatToCallThis = await(getPlayerCoords())
+        for (var i = 0; i<idkWhatToCallThis.length;i++){
+            var player = idkWhatToCallThis[i].split(':')[0];
+            var coords = idkWhatToCallThis[i].split(':')[1];
+            var toPush = {
+                "player": player,
+                "x":coords.split(',')[0],
+                "y":coords.split(',')[1],
+                "z":coords.split(',')[2]
+            }
+            toSend.list.push(toPush)
+        }
+        if (connected){
+            serverConnection.write(JSON.stringify(toSend));
+        }
     }
     if (packet.type=="command"){
-        // Pretty easy, just run packet.command in the server
+        minecraft.stdin.write(packet.command)
     }
 }
 
-// Code for reconnections
 
-serverConnection.on('close',()=>{
-    console.log("Connection to CamelBot lost, reconnecting.")
-    connected=false
-});
-serverConnection.on('error', (data)=>{
-    console.log(data)
-});
-serverConnection.on('data', (data)=>{
-    console.log(Buffer.from(data).toString('ascii'));
-});
-serverConnection.on('connect',()=>{
-    connected=true
-    console.log("Connected to CamelBot")
-    sendMessage("yeet")
-});
+serverGarbage();
 
-console.log("Attempting connection to CamelBot");
+//console.log("Attempting connection to CamelBot");
 connectpls();
 
 setInterval(()=>{
     if (connected==false){
-        console.log("Attempting connection to CamelBot");
+        //console.log("Attempting connection to CamelBot");
+        serverGarbage();
         connectpls();
     }
 },10000);
 
 
+// Once all listeners are removed, they must be called again.
+function serverGarbage(){
+    serverConnection.on('close',()=>{
+        console.log("Connection to CamelBot lost, reconnecting.")
+        // Prevent memory leak, but they are super fun. It's not a mem leak, it's a surprise backup.
+        serverConnection.removeAllListeners();
+        connected=false
+    });
+    serverConnection.on('error', (data)=>{
+        //console.log(data)
+    });
+    serverConnection.on('data', (data)=>{
+        packetInterpreter(data)
+    });
+    serverConnection.on('connect',()=>{
+        connected=true
+        console.log("Connected to CamelBot")
+        sendMessage("yeet")
+    });
+}
 
+
+// Wow, jkcoxson is the best
 
 // Minecraft code
 
 const child_process = require('child_process');
 const { resolve } = require("path");
 const { rejects } = require("assert");
+const { Socket } = require("dgram");
 
 var minecraft = child_process.spawn('java', ['-Xmx4G','-Xms4G','-jar',config.jarname]); 
 minecraft.stdout.setEncoding("utf-8");
@@ -92,10 +134,7 @@ minecraft.stdin.setEncoding("utf-8");
 
 var interpreter = "any"
 
-minecraft.stdout.on('data', async data =>{
-    console.log(data)
-    
-});
+minecraftGarbage();
 
 async function getPlayerCoords(){
     return new Promise((resolve, reject) => {
@@ -104,24 +143,69 @@ async function getPlayerCoords(){
             // This is called forever now... but it doesn't call new ones so little need to worry. 
             // It doesn't resolve unless a function is called.
             if (data.toString().split('\n')[1]=="coordprint"){
-                var toSend = "";
+                var toSend = [];
                 var splitData = data.toString().split('\n');
                 for (var i = 2; i<splitData.length;i++){
                     if (splitData[i]!=='8675309'){
-                        toSend+=splitData[i];
+                        toSend.push(splitData[i]);
                     }
                 }
+                minecraftGarbage();
                 return resolve(toSend);
             }
         });
     });
 }
 
-async function test(){
-    console.log(await getPlayerCoords());
+
+async function getPlayersOnline(){
+    return new Promise((resolve, reject) => {
+        minecraft.stdin.write('playerlist')
+        minecraft.stdout.on('data', async data =>{
+            // This is called forever now... but it doesn't call new ones so little need to worry. 
+            // It doesn't resolve unless a function is called.
+            if (data.toString().split('\n')[1]=="playerlist"){
+                var toSend = "";
+                var splitData = data.toString().split('\n');
+                for (var i = 2; i<splitData.length;i++){
+                    if (splitData[i]!=='8675309'){
+                        toSend+=splitData[i];
+                        toSend+=('\n')
+                    }
+                }
+                // Take out the trash Eddy
+                minecraftGarbage();
+                return resolve(toSend);
+            }
+        });
+    });
 }
 
-test();
+function minecraftGarbage(){
+    minecraft.stdout.removeAllListeners()
+    minecraft.stdout.on('data', async data =>{
+        console.log(data)
+        var toSend = {
+            'packet':'log',
+            'log':data
+        }
+        if (connected){
+            serverConnection.write(JSON.stringify(toSend));
+        }
+        if (data.toString().split(' ')[3].toString().startsWith('<')){
+            var toSend = {
+                'packet':'chat',
+                'sender':data.toString().split(' ')[3].toString().split('<')[1].split('>')[0],
+                'message':data.toString().split(' ').splice(4,data.toString().split(' ').length).join(' ').split('\n')[0]
+            }
+            if (connected){
+                serverConnection.write(JSON.stringify(toSend));
+            }
+        }
+    });
+}
+
+
 
 // [22:45:23] [Server thread/INFO]: [STDOUT]:
 // coordprint
@@ -134,3 +218,4 @@ test();
 // 8675309
 
 // [22:50:31] [Server thread/INFO]: <jkcoxson> Hello World!
+// 33
